408ef0dcb670e605b7cc6c7a893beecb8ffecef8
vu viet <vvviet97@gmail.com>
2022-05-11 11:15:47 +0700

    unit4FindMin

---
+++ unit4/find-min/src/com/codegym/Main.java
@@ -0,0 +1,19 @@
+package com.codegym;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {21,5,47,57,35,64};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
