4e0390d45e0589ce33e03871d37361796b279c06
vu viet <vvviet97@gmail.com>
2022-05-13 16:00:57 +0700

    unit5ClassFan

--- unit5/class-fan/src/com/codegym/Fan.java
+++ unit5/class-fan/src/com/codegym/Fan.java
@@ -1,2 +1,75 @@
-package com.codegym;public class Fan {
+package com.codegym;
+
+public class Fan {
+    final byte SLOW = 1;
+    final byte MEDIUM = 2;
+    final byte FAST = 3;
+
+    private int speed = SLOW;
+    private boolean isOn = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public byte getSLOW() {
+        return SLOW;
+    }
+
+    public byte getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public byte getFAST() {
+        return FAST;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean isOn) {
+        this.isOn = isOn;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Fan() {
+    }
+
+    @Override
+    public String toString() {
+        if (isOn) {
+            return "Fan{ Fan is on: " +
+                    "speed=" + speed +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        } else {
+            return "Fan{ Fan is off: " +
+                    "radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+    }
 }
