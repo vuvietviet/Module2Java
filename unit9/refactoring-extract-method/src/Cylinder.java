3c2810ac8eadf835cd21b79e6995af41bef6b7a4
vu viet <vvviet97@gmail.com>
2022-05-17 08:42:57 +0700

    unit9RefactoringExtractMethod

---
+++ unit9/refactoring-extract-method/src/Cylinder.java
@@ -0,0 +1,22 @@
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = perimeter * height + 2 * baseArea;
+        return volume;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
+    }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        System.out.println(Cylinder.getVolume(2, 3));
+    }
+}
